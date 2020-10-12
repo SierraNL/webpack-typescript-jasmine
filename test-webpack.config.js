0496fafe118e32c48a86aba44e690daff4178ced
Leon Grave <Leon.Grave@vanderlande.com>
2020-10-12 16:11:21 +0200

    LOADER-2 - Updated packages, fixed some warnings, removed some unneeded configuration

--- test-webpack.config.js
+++ test-webpack.config.js
@@ -1,26 +1,18 @@
 var path = require('path');
 
 module.exports = {
-  devtool: '#inline-source-map',
+    mode: 'development',
 
-  entry: [
-    './test/index.spec.ts',
-  ],
+    resolve: {
+        extensions: ['.ts', '.js', '.tsx']
+    },
 
-  output: {
-    filename: 'app/bundle.js'
-  },
-
-  resolve: {
-    extensions: ['.ts', '.js', '.tsx']
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.ts$/, use: [{
-          loader: 'ts-loader'
-        }]
-      }]
-  }
+    module: {
+        rules: [
+            {
+                test: /\.ts$/, use: [{
+                    loader: 'ts-loader'
+                }]
+            }]
+    }
 };
